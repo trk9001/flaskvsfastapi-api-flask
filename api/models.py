b578be9fe2a959f07509952079bee13d9853922d
Sharif M. Tarik <s.tarik@cimplux.com>
2021-03-13 16:40:51 +0600

    Add model and its migration

--- api/models.py
+++ api/models.py
@@ -1,3 +1,35 @@
-from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 from api import db
+
+
+class Product(db.Model):
+    """Model representing a product."""
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(
+        db.String(250),
+        nullable=False,
+    )
+    category = db.Column(
+        db.String(250),
+        nullable=True,
+    )
+    amount = db.Column(
+        db.String(250),
+        nullable=False,
+    )
+    price = db.Column(
+        db.Float(precision=2),
+        nullable=False,
+    )
+    image_source = db.Column(
+        db.String(250),
+        nullable=True,
+    )
+    created_at = db.Column(
+        db.DateTime,
+        default=datetime.now,
+    )
+
+    def __repr__(self):
+        return f'<Product {self.name}>'
